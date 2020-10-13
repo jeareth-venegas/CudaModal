da0764abeda5ca75e0c8b83d7035632f3433b7fa
Jeareth Venegas <venegasjea@gmail.com>
2020-10-12 22:08:40 -0600

    Corrección

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 //div de la ventana
 const windowModal = document.getElementsByClassName("modal");
+
 //boton
 const boton = document.getElementsByClassName("button");
+
 //cerrar
 const cerrar = document.getElementsByClassName("cerrar")[0];
+
 //ocultar la ventana
 windowModal[0].classList.add('hidden');
 
 boton[0].addEventListener('click',()=> {
-    ventanaModal[0].classList.remove('hidden');
+    windowModal[0].classList.remove('hidden');
 })
 
 cerrar.addEventListener('click',()=>{
